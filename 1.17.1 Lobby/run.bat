d2a217584d95acaa2ecd50ad7a9ffc75021358e2
mahdimzl1 <m44168987@gmail.com>
2025-09-06 14:31:12 -0700

    backup 4
    backup 4

--- 1.17.1 Lobby/run.bat
+++ 1.17.1 Lobby/run.bat
@@ -1,3 +1,3 @@
 title lobby
-"C:\Program Files\Java\jdk-17.0.2\bin\java.exe" -Xmx2G -Xmx2G -jar paper-1.17.1-411.jar nogui
+"C:\Program Files\Java\jdk-17.0.2\bin\java.exe" -Xmx6G -Xmx6G -jar paper-1.17.1-411.jar nogui
 PAUSE
--- 1.20.6 Survive/run.bat
+++
@@ -1,3 +0,0 @@
-title survive
-"C:\Program Files\Java\jdk-21\bin\java.exe" -Xmx2G -Xmx2G -jar paper-1.20.6-151.jar nogui
-PAUSE
